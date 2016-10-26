917e01dd517e38fe7f6b519bc6b73a34f4d348cc
federico <f.fanalista@tudelft.nl>
2016-10-26 12:08:29 +0000

    hey wayne, go back to your fucking country

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-print('hello world')
-import math 
-    
-def circumference(x,output):
-    %This function calculates the circumference of a circle
-    output=2*pi*x
-    
-    %test 
-    
-    print('Hello Fede')
+hello wayne
